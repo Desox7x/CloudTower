ce7dfb934e07d0fdfe610f7de3327ab2f5cbc3c5
AbdelG <agenao1997@gmail.com>
2019-08-16 14:11:56 -0400

    'configuracion'

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const exhandle = require('express-handlebars');
+const path = require('path');
+
+//Iniciar
+const app = express();
+
+//Configuracion
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exhandle({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Variables globales
+app.use((req, res, next) => {
+    next();
+});
+
+//Rutas
+app.use(require('./routes/rutas.js'))
+app.use(require('./routes/authentication.js'))
+app.use('/links', require('./routes/links.js'))
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Iniciar Server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
