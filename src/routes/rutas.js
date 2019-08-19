42ccc120a8c8ff484be7a12c9692a4028bc0c2ac
AbdelG <agenao1997@gmail.com>
2019-08-19 19:04:18 -0400

    Agregado de disenos

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('./layouts/inicio')
+});
+router.get('/about', (req, res) => {
+    res.render('./layouts/about');
 });
 
 module.exports = router;
