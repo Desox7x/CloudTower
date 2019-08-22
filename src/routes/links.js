5f51241b53f33a8b6f6e2c18f3b20f9fc83da3f8
unknown <cargandocloromorfo@hotmail.com>
2019-08-22 17:39:29 -0400

    contrato

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,6 +5,9 @@ const pool = require('../database');
 router.get('/dashboard', (req, res) => {
     res.render('postlog/dashboard');
 });
+router.get('/contract', (req, res) => {
+    res.render('postlog/contract');
+});
 
 
 module.exports = router;
